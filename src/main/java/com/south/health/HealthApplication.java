819b7044dd2da75552f0a6eccb475246b92d6e7b
Borba <gabriel.lewis@acad.pucrs.bbr>
2021-10-26 12:21:44 -0300

    Hello World

--- src/main/java/com/south/health/HealthApplication.java
+++ src/main/java/com/south/health/HealthApplication.java
@@ -2,12 +2,20 @@ package com.south.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class HealthApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthApplication.class, args);
 	}
-
 }
